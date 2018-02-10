0500bb3cb550fa5a2289ae98d5f4f044166d4848
markgenisis <sabilla.genisis@yahoo.com>
2018-02-10 14:11:27 +0800

    aa

---
+++ cashier/cashvoucher.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Cash Voucher</title>
+</head>
+
+<body>
+<table border="1" width="900" style="border:1px solid #000; border-collapse:collapse;">
+	<tr>
+    
+    	<td colspan="3" style="text-align:center" width="600">CASH VOUCHER:</td> 
+    </tr>
+    <tr>
+    
+    	<td rowspan="2" width="600">Payee:</td>
+        <td colspan="2">No:</td>
+    </tr>
+    <tr>
+    	 
+        <td colspan="2">Date:</td>
+    </tr>
+	<tr style="text-align:center;">
+    	<td>PARTICULARS</td>
+        <td width="150">AMOUNT</td>
+        <td width="100">TOTAL</td>
+    </tr>
+    <tr>	
+    	<td style="height:300px;" rowspan="2"></td>
+        <td  style="height:270px;"></td>
+        <td style="height:270px;"></td>
+    </tr>	
+    <tr>
+    	<td style="width:100px;"></td>
+        <td></td>
+    </tr>
+</table>
+<table border="1" width="900" style="border:1px solid #000; border-collapse:collapse;">
+	 
+    <tr>
+    	<td width="200" style="text-align:center;" colspan="2">Approved by:</td>
+         
+        <td width="400" colspan="2" style="top:0px;"  >Received Payment by:<br /></td>
+    </tr>
+    <tr>
+    	<td width="200" style="text-align:center;">&nbsp; </td>
+        
+         <td style="text-align:center;" colspan="2"> </td>
+         
+    </tr>
+    <tr>
+    	<td width="200" style="text-align:center;" >&nbsp; </td>
+        
+         <td style="text-align:center;" colspan="2" rowspan="2"> </td>
+         
+    </tr>
+    <tr>
+    	<td width="200" style="text-align:center;">&nbsp; </td>
+        
+          
+         
+    </tr>
+</table>
+</body>
+</html>
