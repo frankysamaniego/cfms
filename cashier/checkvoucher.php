50939e1059dc029291f70c4597c6678ffc1d68f4
markgenisis <sabilla.genisis@yahoo.com>
2018-02-10 13:47:24 +0800

    aaa

--- cashier/checkvoucher.php
+++ cashier/checkvoucher.php
@@ -2,9 +2,74 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>Chech Voucher</title>
 </head>
 
 <body>
+<table border="1" width="900" style="border:1px solid #000; border-collapse:collapse;">
+	<tr>
+    	<td rowspan="2" width="600"></td>
+        <td colspan="2">No:</td>
+    </tr>
+    <tr>
+    	 
+        <td colspan="2">Date:</td>
+    </tr>
+	<tr style="text-align:center;">
+    	<td>PARTICULARS</td>
+        <td width="150">AMOUNT</td>
+        <td width="100">TOTAL</td>
+    </tr>
+    <tr>	
+    	<td style="height:300px;" rowspan="2"></td>
+        <td  style="height:270px;"></td>
+        <td style="height:270px;"></td>
+    </tr>	
+    <tr>
+    	<td style="width:100px;"></td>
+        <td></td>
+    </tr>
+</table>
+<table border="1" width="900" style="border:1px solid #000; border-collapse:collapse;">
+	<tr>
+    	<td width="200" style="text-align:center;">ACCOUNT TITLE</td>
+        <td style="text-align:center;" width="300">DEBIT</td>
+        <td style="text-align:center;" width="300">CREDIT</td>
+        <td width="400" colspan="2">Pesos:</td>
+    </tr>
+    <tr>
+    	<td width="200" style="text-align:center;">&nbsp; </td>
+        <td style="text-align:center;"> </td>
+        <td style="text-align:center;"> </td>
+        <td width="400" colspan="2"> </td>
+    </tr>
+    <tr>
+    	<td width="200" style="text-align:center;">&nbsp; </td>
+        <td style="text-align:center;"> </td>
+        <td style="text-align:center;"> </td>
+        <td width="400">Bank: </td>
+         <td width="400">Bank: </td>
+    </tr>
+    <tr>
+    	<td width="200" style="text-align:center;">&nbsp; </td>
+        <td style="text-align:center;"> </td>
+        <td style="text-align:center;"> </td>
+        <td width="400"> </td>
+         <td width="400">Bank: </td>
+    </tr>
+    <tr>
+    	<td width="200" style="text-align:center;">Prepared by:</td>
+        <td style="text-align:center;" width="300">Certified Correct by:</td>
+        <td style="text-align:center;" width="300">Approved by:</td>
+        <td width="400" colspan="2" style="top:0px;"  >Received Payment by:<br /></td>
+    </tr>
+    <tr>
+    	<td width="200" style="text-align:center;">&nbsp; </td>
+        <td style="text-align:center;"> </td>
+        <td style="text-align:center;"> </td>
+         <td style="text-align:center;" colspan="2"> </td>
+         
+    </tr>
+</table>
 </body>
 </html>
